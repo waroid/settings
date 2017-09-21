d582ab2b17472cc4577b21a30f48a62fbee24d53
mirime <kmirime@gmail.com>
2017-09-21 13:26:56 +0900

    --

--- 2/updateconfig.sh
+++ 2/updateconfig.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "copy config"
+echo "copy wpa_supplicant.conf"
 sudo cp etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+echo "copy interfaces"
 sudo cp etc/network/interfaces /etc/network/interfaces
+echo "copy rc.local"
 sudo cp etc/rc.local /etc/rc.local
 
 echo "Reboot to update config"
